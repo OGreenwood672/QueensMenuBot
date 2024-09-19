4db892200cb5db24efad01e26127ba6b5590acaf
Greenwood <greenwood672.dev@gmail.com>
2024-09-19 21:49:30 +0100

    Add test

--- api/index.py
+++ api/index.py
@@ -88,8 +88,9 @@ def internal_server_error(e):
 
 @app.route('/')
 def index():
-    fb_login_url = f"https://www.facebook.com/v20.0/dialog/oauth?client_id={FB_APP_ID}&redirect_uri={REDIRECT_URI_CODE}&scope=pages_manage_posts,instagram_content_publish"
-    return redirect(fb_login_url)
+    # fb_login_url = f"https://www.facebook.com/v20.0/dialog/oauth?client_id={FB_APP_ID}&redirect_uri={REDIRECT_URI_CODE}&scope=pages_manage_posts,instagram_content_publish"
+    # return redirect(fb_login_url)
+    return "Queens' Menu Bot"
 
 @app.route('/validate-code')
 def validate_code():
